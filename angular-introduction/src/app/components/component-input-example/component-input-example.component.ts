5ee2d0040d5b19d6e96141196c6eb6296215f1f9
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-04-28 18:35:08 +0300

    Angular navbar

--- angular-introduction/src/app/components/component-input-example/component-input-example.component.ts
+++ angular-introduction/src/app/components/component-input-example/component-input-example.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Person } from 'src/app/shared/interfaces/person';
 
 @Component({
   selector: 'app-component-input-example',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './component-input-example.component.css'
 })
 export class ComponentInputExampleComponent {
+  person0: Person = {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 30,
+    email: 'j.doe@example.com',
+    address: '123 Main St, Cityville, Country'
+  }
 
+  person1: Person = {
+    givenName: 'Jane',
+    surName: 'Smith',
+    age: 25,
+    email: 'janesmith@example.edu',
+    address: '456 Elm St, Townsville, Country'
+  }
 }
