a42ab8b000cd3219477731eed0d18b6429d0c447
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-05-12 21:05:37 +0300

    updates

--- angular-introduction/src/app/shared/services/user.service.ts
+++ angular-introduction/src/app/shared/services/user.service.ts
@@ -14,4 +14,9 @@ export class UserService {
   registerUser(user: User) {
     return this.http.post<{status: boolean, data: User}>(`${API_URL}`, user);
   }
+
+  checkDuplicateEmail(email: string) {
+    return this.http.get<{status: boolean, data: User}>
+    (`${API_URL}/checkDuplicateEmail/${email}`);
+  }
 }
